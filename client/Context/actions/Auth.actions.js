fe84b704f1b9a6323d58e6dea0d7adae1e18e3dc
khj <ase9810@naver.com>
2021-03-05 14:36:28 +0900

    로그인 수정

--- client/Context/actions/Auth.actions.js
+++ client/Context/actions/Auth.actions.js
@@ -6,59 +6,66 @@ import baseURL from "../../assets/common/baseURL";
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
 
 export const loginUser = (user, dispatch) => {
-	fetch(`${baseURL}users/login`, {
-		method: "POST",
-		body: JSON.stringify(user),
-		headers: {
-			Accept: "application/json",
-			"Content-Type": "application/json",
-		},
-	})
-		.then((res) => res.json())
-		.then((data) => {
-			if (data) {
-				const token = data.token;
-				AsyncStorage.setItem("jwt", token);
-				const decoded = jwt_decode(token);
-				dispatch(setCurrentUser(decoded, user));
-			} else {
-				logoutUser(dispatch);
-			}
-		})
-		.catch((err) => {
-			Toast.show({
-				topOffset: 60,
-				type: "error",
-				text1: "Please provide correct credentials",
-				text2: "",
-			});
-			console.log(err);
-			logoutUser(dispatch);
-		});
+  fetch(`${baseURL}users/login`, {
+    method: "POST",
+    body: JSON.stringify(user),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data) {
+        const token = data.token;
+        AsyncStorage.setItem("jwt", token);
+        const decoded = jwt_decode(token);
+        dispatch(setCurrentUser(decoded, user));
+        Toast.show({
+          topOffset: 60,
+          type: "success",
+          text1: "Login Successful!!!",
+          autoHide: true,
+          visibilityTime: 1000,
+        });
+      } else {
+        logoutUser(dispatch);
+      }
+    })
+    .catch((err) => {
+      Toast.show({
+        topOffset: 60,
+        type: "error",
+        text1: "Please provide correct credentials",
+        text2: "",
+      });
+      console.log(err);
+      logoutUser(dispatch);
+    });
 };
 
 export const getUserProfile = (id) => {
-	fetch(`${baseURL}users/${id}`, {
-		method: "GET",
-		body: JSON.stringify(user),
-		headers: {
-			Accept: "application/json",
-			"Content-Type": "application/json",
-		},
-	})
-		.then((res) => res.json())
-		.then((data) => console.log(data));
+  fetch(`${baseURL}users/${id}`, {
+    method: "GET",
+    body: JSON.stringify(user),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 };
 
 export const logoutUser = (dispatch) => {
-	AsyncStorage.removeItem("jwt");
-	dispatch(setCurrentUser({}));
+  AsyncStorage.removeItem("jwt");
+  dispatch(setCurrentUser({}));
 };
 
 export const setCurrentUser = (decoded, user) => {
-	return {
-		type: SET_CURRENT_USER,
-		payload: decoded,
-		userProfile: user,
-	};
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded,
+    userProfile: user,
+  };
 };
--- client/Screens/User/Login.js
+++ client/Screens/User/Login.js
@@ -1,5 +1,12 @@
-import React, { useCallback, useContext, useEffect, useState } from "react";
-import { View, Text, StyleSheet, ScrollView, Button, ActivityIndicator } from "react-native";
+import React, { useCallback, useContext, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Button,
+  ActivityIndicator,
+} from "react-native";
 import FormContainer from "../../Shared/Form/FormContainer";
 import Input from "../../Shared/Form/Input";
 import Error from "../../Shared/Error";
@@ -18,178 +25,181 @@ import OrderCard from "../../Shared/OrderCard";
 import { useFocusEffect } from "@react-navigation/native";
 
 const Login = (props) => {
-	const context = useContext(AuthGlobal);
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const [error, setError] = useState("");
+  const context = useContext(AuthGlobal);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-	const [userProfile, setUserProfile] = useState();
-	const [orders, setOrders] = useState();
-	const [loading, setLoading] = useState(true);
-	const [focus, setFocus] = useState(true)
+  const [userProfile, setUserProfile] = useState();
+  const [orders, setOrders] = useState();
+  const [loading, setLoading] = useState(true);
+  const [focus, setFocus] = useState(true);
 
-	useFocusEffect(
-		useCallback(() => {
-			setFocus(false)
-			if (context.stateUser.isAuthenticated) {
-				AsyncStorage.getItem("jwt")
-					.then((res) => {
-						axios
-							.get(`${baseURL}users/${context.stateUser.user.userId}`, {
-								headers: { Authorization: `Bearer ${res}` },
-							})
-							.then((user) => setUserProfile(user.data))
-							.catch((err) => console.log(err));
-					})
-					.catch((error) => console.log(error));
+  useFocusEffect(
+    useCallback(() => {
+      setFocus(false);
+      if (context.stateUser.isAuthenticated) {
+        AsyncStorage.getItem("jwt")
+          .then((res) => {
+            axios
+              .get(`${baseURL}users/${context.stateUser.user.userId}`, {
+                headers: { Authorization: `Bearer ${res}` },
+              })
+              .then((user) => setUserProfile(user.data))
+              .catch((err) => console.log(err));
+          })
+          .catch((error) => console.log(error));
 
-				axios
-					.get(`${baseURL}orders`)
-					.then((x) => {
-						const data = x.data;
-						const userOrders = data.filter((order) => order.user._id === context.stateUser.user.userId);
-						setOrders(userOrders);
-						setLoading(false);
-					})
-					.catch((error) => console.log(error));
+        axios
+          .get(`${baseURL}orders`)
+          .then((x) => {
+            const data = x.data;
+            const userOrders = data.filter(
+              (order) => order.user._id === context.stateUser.user.userId
+            );
+            setOrders(userOrders);
+            setLoading(false);
+          })
+          .catch((error) => console.log(error));
 
-				return () => {
-					setUserProfile();
-					setOrders();
-					setLoading(true);
-					setFocus(true)
-				};
-			}
-		}, [focus])
-	);
+        return () => {
+          setUserProfile();
+          setOrders();
+          setLoading(true);
+          setFocus(true);
+        };
+      }
+    }, [focus, context.stateUser.isAuthenticated])
+  );
 
-	const handleSubmit = () => {
-		const user = {
-			email,
-			password,
-		};
+  console.log(context.stateUser);
 
-		if (email === "" || password === "") {
-			setError("Please fill in your credentials");
-		} else {
-			loginUser(user, context.dispatch);
-			Toast.show({
-				topOffset: 60,
-				type: "success",
-				text1: "Login Successful!!!",
-				autoHide: true,
-				visibilityTime: 1000,
-			});
-			props.navigation.navigate("Home");
-		}
-	};
+  const handleSubmit = () => {
+    const user = {
+      email,
+      password,
+    };
 
-	console.log(context.stateUser.userProfile);
-	console.log(userProfile);
+    if (email === "" || password === "") {
+      setError("Please fill in your credentials");
+    } else {
+      loginUser(user, context.dispatch);
+    }
+  };
 
-	return (
-		<>
-			{context.stateUser.isAuthenticated === true ? (
-				<Container style={styles.container}>
-					<ScrollView contentContainerStyle={styles.subContainer}>
-						<Text style={{ fontSize: 30 }}>{userProfile ? userProfile.name : ""}</Text>
-						<View style={{ marginTop: 20 }}>
-							<Text style={{ margin: 10 }}>Email: {userProfile ? userProfile.email : ""}</Text>
-							<Text style={{ margin: 10 }}>Phone: {userProfile ? userProfile.phone : ""}</Text>
-						</View>
-						<View style={{ marginTop: 80 }}>
-							<Button
-								title={"Sign Out"}
-								onPress={() => [
-									AsyncStorage.removeItem("jwt"),
-									logoutUser(context.dispatch),
-									Toast.show({
-										topOffset: 60,
-										type: "success",
-										text1: "Logout Successful!!!",
-										autoHide: true,
-										visibilityTime: 1000,
-									}),
-								]}
-							/>
-						</View>
-						{!loading ? (
-							<View style={styles.order}>
-								<Text style={{ fontSize: 20 }}>My Orders</Text>
-								<View>
-									{orders ? (
-										orders.map((x) => {
-											return <OrderCard key={x.id} {...x} />;
-										})
-									) : (
-										<View style={styles.order}>
-											<Text>You have no orders</Text>
-										</View>
-									)}
-								</View>
-							</View>
-						) : (
-							<ActivityIndicator size="large" color="red" />
-						)}
-					</ScrollView>
-				</Container>
-			) : (
-				<FormContainer title={"Login"}>
-					<Input
-						placeholder={"Enter Email"}
-						name={"email"}
-						id={"email"}
-						value={email}
-						onChangeText={(text) => setEmail(text.toLowerCase())}
-					/>
-					<Input
-						placeholder={"Enter Password"}
-						name={"password"}
-						id={"password"}
-						secureTextEntry={true}
-						value={password}
-						onChangeText={(text) => setPassword(text)}
-					/>
-					<View style={styles.buttonGroup}>
-						{error ? <Error message={error} /> : null}
-						<EasyButton large primary onPress={() => handleSubmit()}>
-							<Text style={{ color: "white" }}>Login</Text>
-						</EasyButton>
-					</View>
-					<View style={[{ marginTop: 40 }, styles.buttonGroup]}>
-						<Text style={styles.middleText}>Don't have an account yet?</Text>
-						<EasyButton large secondary onPress={() => props.navigation.navigate("Register")}>
-							<Text style={{ color: "white" }}>Register</Text>
-						</EasyButton>
-					</View>
-				</FormContainer>
-			)}
-		</>
-	);
+  return (
+    <>
+      {context.stateUser.isAuthenticated === true ? (
+        <Container style={styles.container}>
+          <ScrollView contentContainerStyle={styles.subContainer}>
+            <Text style={{ fontSize: 30 }}>
+              {userProfile ? userProfile.name : ""}
+            </Text>
+            <View style={{ marginTop: 20 }}>
+              <Text style={{ margin: 10 }}>
+                Email: {userProfile ? userProfile.email : ""}
+              </Text>
+              <Text style={{ margin: 10 }}>
+                Phone: {userProfile ? userProfile.phone : ""}
+              </Text>
+            </View>
+            <View style={{ marginTop: 80 }}>
+              <Button
+                title={"Sign Out"}
+                onPress={() => [
+                  AsyncStorage.removeItem("jwt"),
+                  logoutUser(context.dispatch),
+                  Toast.show({
+                    topOffset: 60,
+                    type: "success",
+                    text1: "Logout Successful!!!",
+                    autoHide: true,
+                    visibilityTime: 1000,
+                  }),
+                ]}
+              />
+            </View>
+            {!loading ? (
+              <View style={styles.order}>
+                <Text style={{ fontSize: 20 }}>My Orders</Text>
+                <View>
+                  {orders ? (
+                    orders.map((x) => {
+                      return <OrderCard key={x.id} {...x} />;
+                    })
+                  ) : (
+                    <View style={styles.order}>
+                      <Text>You have no orders</Text>
+                    </View>
+                  )}
+                </View>
+              </View>
+            ) : (
+              <ActivityIndicator size="large" color="red" />
+            )}
+          </ScrollView>
+        </Container>
+      ) : (
+        <FormContainer title={"Login"}>
+          <Input
+            placeholder={"Enter Email"}
+            name={"email"}
+            id={"email"}
+            value={email}
+            onChangeText={(text) => setEmail(text.toLowerCase())}
+          />
+          <Input
+            placeholder={"Enter Password"}
+            name={"password"}
+            id={"password"}
+            secureTextEntry={true}
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+          />
+          <View style={styles.buttonGroup}>
+            {error ? <Error message={error} /> : null}
+            <EasyButton large primary onPress={() => handleSubmit()}>
+              <Text style={{ color: "white" }}>Login</Text>
+            </EasyButton>
+          </View>
+          <View style={[{ marginTop: 40 }, styles.buttonGroup]}>
+            <Text style={styles.middleText}>Don't have an account yet?</Text>
+            <EasyButton
+              large
+              secondary
+              onPress={() => props.navigation.navigate("Register")}
+            >
+              <Text style={{ color: "white" }}>Register</Text>
+            </EasyButton>
+          </View>
+        </FormContainer>
+      )}
+    </>
+  );
 };
 
 const styles = StyleSheet.create({
-	buttonGroup: {
-		width: "80%",
-		alignItems: "center",
-	},
-	middleText: {
-		marginBottom: 20,
-		alignSelf: "center",
-	},
-	container: {
-		flex: 1,
-		alignItems: "center",
-	},
-	subContainer: {
-		alignItems: "center",
-		marginTop: 60,
-	},
-	order: {
-		marginTop: 20,
-		alignItems: "center",
-		marginBottom: 60,
-	},
+  buttonGroup: {
+    width: "80%",
+    alignItems: "center",
+  },
+  middleText: {
+    marginBottom: 20,
+    alignSelf: "center",
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  subContainer: {
+    alignItems: "center",
+    marginTop: 60,
+  },
+  order: {
+    marginTop: 20,
+    alignItems: "center",
+    marginBottom: 60,
+  },
 });
 
 export default Login;
